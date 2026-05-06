e8286a77468f3b339723b69a73b24f92cf38165d
0135494 <Chih09mck@ya.ru>
2026-05-06 21:31:52 +0500

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+git init
+git remote add origin https://github.com/0135494/android-poultry-monitor.git
+git add .
+git commit -m "feat: initial Android MVVM + Room + Compose project for poultry management"
+git branch -M main
+git push -u origin main
