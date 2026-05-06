c704edd5f6a00c54afbdac3f51495077aa0ddc89
0135494 <Chih09mck@ya.ru>
2026-05-06 21:32:45 +0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,2 @@
-git init
-git remote add origin https://github.com/0135494/android-poultry-monitor.git
-git add .
-git commit -m "feat: initial Android MVVM + Room + Compose project for poultry management"
-git branch -M main
-git push -u origin main
+rootProject.name = "android-poultry-monitor"
+include(":app")
