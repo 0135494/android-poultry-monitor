ffe1d7ed1731241eeb898d403ae1fc18170ac682
0135494 <Chih09mck@ya.ru>
2026-05-07 12:52:08 +0500

    Create VetEventDao.kt

---
+++ app/src/main/java/com/yourfarm/app/data/dao/VetEventDao.kt
@@ -0,0 +1,16 @@
+package com.yourfarm.app.data.dao
+
+import androidx.room.*
+import com.yourfarm.app.data.entity.VetEvent
+
+@Dao
+interface FlockDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFlock(flock: VetEvent)
+
+    @Query("SELECT * FROM parent_flocks WHERE id = :id")
+    suspend fun getFlock(id: String): VetEvent?
+
+    @Query("SELECT * FROM parent_flocks")
+    suspend fun getAllFlocks(): List<VetEvent>
+}
