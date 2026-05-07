e8a0668edca722b485da61c2f30d952ff20973ef
0135494 <Chih09mck@ya.ru>
2026-05-07 12:54:46 +0500

    Create MicroclimateDao.kt

---
+++ app/src/main/java/com/yourfarm/app/data/dao/MicroclimateDao.kt
@@ -0,0 +1,16 @@
+package com.yourfarm.app.data.dao
+
+import androidx.room.*
+import com.yourfarm.app.data.entity.Microclimate
+
+@Dao
+interface MicroclimateDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFlock(flock: Microclimate)
+
+    @Query("SELECT * FROM Microclimate WHERE id = :id")
+    suspend fun get Microclimate(id: String): Microclimate?
+
+    @Query("SELECT * FROM Microclimate")
+    suspend fun getAllFlocks(): List<Microclimate>
+}
