e6ae4520775e67eb83a233dbf898903637f65031
0135494 <Chih09mck@ya.ru>
2026-05-14 09:00:11 +0500

    Update ConsumptionDao.kt

--- app/src/main/java/com/yourfarm/app/data/dao/ConsumptionDao.kt
+++ app/src/main/java/com/yourfarm/app/data/dao/ConsumptionDao.kt
@@ -8,9 +8,9 @@ interface ConsumptionDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFlock(flock: Consumption)
 
-    @Query("SELECT * FROM Consumption WHERE id = :id")
+    @Query("SELECT * FROM parent_flocks WHERE id = :id")
     suspend fun get Consumption(id: String): Consumption?
 
-    @Query("SELECT * FROM Consumption")
+    @Query("SELECT * FROM parent_flocks")
     suspend fun getAllFlocks(): List<Consumption>
 }
